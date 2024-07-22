e8ddbf4a3402e02f6d12342b0447ae235a354717
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-22 12:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+    if (!s) {
+        return '';
+    }
+    s = s.trim();
+    const firstSpaceIndex = s.indexOf(' ');
+    if (firstSpaceIndex === -1) {
+        return s;
+    }
+    return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
